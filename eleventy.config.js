66a7d329c1473605df75abd387e641fb39c7c5d0
Daniel Jankowski <daniell.jankowskii@gmail.com>
2025-05-14 18:24:22 +0200

    Publish the first blog post

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,5 +1,6 @@
 import pluginNavigation from "@11ty/eleventy-navigation";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 import pluginFilters from "./_config/filters.js";
 
@@ -21,6 +22,20 @@ export default function (eleventyConfig) {
     preAttributes: { tabindex: 0 },
   });
   eleventyConfig.addPlugin(pluginNavigation);
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		formats: ["avif", "webp", "auto"],
+		failOnError: false,
+		htmlOptions: {
+			imgAttributes: {
+				loading: "lazy",
+				decoding: "async",
+			}
+		},
+
+		sharpOptions: {
+			animated: true,
+		},
+	});
   eleventyConfig.addPlugin(pluginFilters);
 }
 
